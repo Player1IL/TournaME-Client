5deaf5e4d207b397bf104f04b87828cd0f1e2f29
Daniel <91071131+Player1IL@users.noreply.github.com>
2024-07-17 19:11:44 +0300

    Added Utils for commonly used functions

---
+++ src/app/functions/Utils.js
@@ -0,0 +1,26 @@
+import {hash} from "bcrypt";
+
+export async function validatePasswords(password, passwordRepeat) {
+    if (password === passwordRepeat) {
+        const hashedPassword = await hash(password, 10);
+        return true
+    }
+    return false
+}
+export function validateDob(dob) {
+    const dobF = new Date(dob);
+    const today = new Date();
+
+    let age = today.getFullYear() - dobF.getFullYear();
+    const monthDiff = today.getMonth() - dobF.getMonth();
+    const dayDiff = today.getDate() - dobF.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+    return age >= 13;
+}
+export function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
