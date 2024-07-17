f6d85bbb204a67b5fbe954c5f53c7b3275474cba
Daniel <91071131+Player1IL@users.noreply.github.com>
2024-07-17 19:15:05 +0300

    Added route for signup

---
+++ src/app/api/auth/signup/route.js
@@ -0,0 +1,21 @@
+import {NextResponse} from "next/server";
+import {validateDob, validateEmail, validatePasswords} from "@/app/functions/Utils.js";
+
+export async function POST(request) {
+    try {
+        const {email, username, password, passwordRepeat, dob} = await request.json();
+
+        // Need to check if username is in db
+        // Need to edit Email validation to check if email not in db
+        if (validateEmail(email) && await validatePasswords(password, passwordRepeat) && validateDob(dob)) {
+            console.log("ALL VALID");
+        } else {
+            console.log("ERROR");
+        }
+
+        console.log({ email, username, password, passwordRepeat, dob });
+    } catch (e) {
+        console.error(e);
+    }
+    return NextResponse.json({ message: "success" });
+}
