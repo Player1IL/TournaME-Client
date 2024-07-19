e11f6f149a5bdc1ed679ed13818fdc799a8fcb52
Daniel <91071131+Player1IL@users.noreply.github.com>
2024-07-19 15:43:20 +0300

    Removed session

--- src/app/pages/signin/page.js
+++ src/app/pages/signin/page.js
@@ -4,10 +4,12 @@ import {getServerSession} from "next-auth";
 import {redirect} from "next/navigation";
 
 export default async function SignIn() {
+    /*
     const session = await getServerSession()
     if (session) {
         redirect("/")
     }
+     */
     return (
         <Form/>
     );
