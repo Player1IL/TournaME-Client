159ccfc509320d35a5937b9dcef82fe122a3b714
Daniel <91071131+Player1IL@users.noreply.github.com>
2024-07-19 15:26:33 +0300

    Changed fetch to cloud server

--- src/app/pages/signin/form.js
+++ src/app/pages/signin/form.js
@@ -21,7 +21,7 @@ export default function Form() {
         });
         console.log(response);
          */
-        const response = await fetch('http://localhost:3124/login', {
+        const response = await fetch('https://tourname.onrender.com/login', {
             method: 'POST',
             body: JSON.stringify({username: formData.get("username"), password: formData.get("password")}),
             headers: {
