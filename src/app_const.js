44e30511d520c3f17df1c48594593cb47e94c51b
GalDeri <118122377+GalDeri@users.noreply.github.com>
2024-08-17 19:29:28 +0300

    Update app_const.js

--- src/app_const.js
+++ src/app_const.js
@@ -1,5 +1,5 @@
 const LOCAL = 'http://localhost:3124';
 const DEPLOY = 'https://tourname.onrender.com';
 const DEV = 'https://tourname-server-side-dev.onrender.com';
-export const IP = LOCAL;
+export const IP = DEV;
 
